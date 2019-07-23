1e444c063a43d16ba1d89e33a235c0707ae94433
Erick <sevieyra@gmail.com>
2019-07-23 23:12:55 +0200

    created nav bar and buttons component

---
+++ src/components/LoginButtons.vue
@@ -0,0 +1,3 @@
+<template>
+    <div>Buttons :D</div>
+</template>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <div class="navbar-item">
+        <h1 class="title">{{title}}</h1>
+      </div>
+    </div>
+    <div class="navbar-menu">
+      <div class="navbar-start"></div>
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <LoginButtons />
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+import LoginButtons from "./LoginButtons.vue";
+
+export default {
+  components: { LoginButtons },
+  data: function() {
+    return {
+      title: "Firebase-vue-pwa"
+    };
+  }
+};
+</script>
+
