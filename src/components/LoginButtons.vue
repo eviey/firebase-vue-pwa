0b501ce30c873214f9579f6f7cb9f11fdac7dd0a
Erick <sevieyra@gmail.com>
2019-07-24 22:25:36 +0200

    corrected log in buttons

--- src/components/LoginButtons.vue
+++ src/components/LoginButtons.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="buttons">
-         <b-button type="is-outlined is-primary">Log-in</b-button>
-         <b-button type="is-primary">Sign-in</b-button>
+         <b-button type="is-outlined is-primary">Log in</b-button>
+         <b-button type="is-primary">Sign up</b-button>
     </div>
 </template>
