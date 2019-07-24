5596e83eb30babc6d2d4b34bf2f31d55a9548955
Erick <sevieyra@gmail.com>
2019-07-24 21:18:10 +0200

    added shadow plus do-narrow class

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,19 +1,21 @@
 <template>
-  <nav class="navbar" role="navigation" aria-label="main navigation">
-    <div class="navbar-brand">
-      <div class="navbar-item">
-        <h1 class="title">{{title}}</h1>
-      </div>
-    </div>
-    <div class="navbar-menu">
-      <div class="navbar-start"></div>
-      <div class="navbar-end">
+  <div class="shadow">
+    <nav class="navbar do-narrow" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
         <div class="navbar-item">
-          <LoginButtons />
+          <h1 class="title">{{title}}</h1>
         </div>
       </div>
-    </div>
-  </nav>
+      <div class="navbar-menu">
+        <div class="navbar-start"></div>
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <LoginButtons />
+          </div>
+        </div>
+      </div>
+    </nav>
+  </div>
 </template>
 
 <script>
@@ -28,4 +30,18 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+
+.shadow {
+  box-shadow: 0px 2px 5px 3px #ccc;
+}
+
+.do-narrow {
+  margin: 0 auto;
+  max-width: 80rem;
+}
+
+</style>
+
 
