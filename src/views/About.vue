5602f592bee73d242e0cb3b20decb1231adbbc17
Erick Vieyra <sevieyra@gmail.com>
2019-07-25 09:11:29 +0200

    modified about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h2>🧾 About firebase-vue-pwa</h2>
+    <p>
+      Thanks for using this template. This is a work in progress that I hope you find useful in your projects.
+    </p>
+    <h3>💬 Contact</h3>
+    <h4>- Erick Vieyra</h4>
+    <ul>
+    <li>📧 Email: <a href="mailto:sevieyra@gmail.com">sevieyra@gmail.com</a>.</li>
+    <li>💻 Github: <a href="https://github.com/severickbot/firebase-vue-pwa">/severickbot</a>.</li>
+    <li>💼 LinkedIn: <a href="https://linkedin.com/in/sevieyra">/sevieyra</a>.</li>
+    </ul>
   </div>
 </template>
