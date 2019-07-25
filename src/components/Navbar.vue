6667ca928dbb131b5884c91cb12d83a1d1eac121
Erick Vieyra <sevieyra@gmail.com>
2019-07-25 08:40:45 +0200

    basic navbar menu

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,9 +5,16 @@
       <div class="navbar-item">
         <h1 class="title">{{title}}</h1>
       </div>
+      <a role="button" class="navbar-burger burger" id="navbar-burger" @click="toggleMenu">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
     </div>
-    <div class="navbar-menu">
-      <div class="navbar-start"></div>
+    <div class="navbar-menu" id="navbar-menu">
+      <div class="navbar-start">
+        <router-link v-for="(item,i) in menu" :key="i" class="navbar-item" :to="item.route">{{item.label}}</router-link>
+      </div>
       <div class="navbar-end">
         <div class="navbar-item">
           <LoginButtons />
@@ -25,8 +32,18 @@ export default {
   components: { LoginButtons },
   data: function() {
     return {
-      title: "Firebase-vue-pwa"
+      title: "Firebase-vue-pwa",
+      menu : [
+        {label: 'start', route: '/'},
+        {label: 'about', route: '/about'}
+      ]
     };
+  },
+  methods: {
+    toggleMenu: function (){
+      document.getElementById('navbar-menu').classList.toggle('is-active')
+      document.getElementById('navbar-burger').classList.toggle('is-active')
+    }
   }
 };
 </script>
