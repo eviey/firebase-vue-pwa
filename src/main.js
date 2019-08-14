fbe5d6ac315da6c547d52b29f6cac1c155edbb6f
Erick Vieyra <erick.vieyra@asml.com>
2019-08-14 16:28:35 +0200

    added brand icons

--- src/main.js
+++ src/main.js
@@ -8,9 +8,10 @@ import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core';
+import { faFacebookSquare, faGoogle } from '@fortawesome/free-brands-svg-icons';
 import { faBars /*  add more icons here */} from '@fortawesome/free-solid-svg-icons'
 
-library.add(faBars)
+library.add(faBars, faFacebookSquare, faGoogle)
 Vue.component('vue-fontawesome', FontAwesomeIcon)
 Vue.use(Vuex)
 Vue.use(Buefy, {
